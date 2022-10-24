bb0346a3f1fd79971239ab4083dcb999c855c3fb
MasonFraley <masonfraley@hotmail.com>
2022-10-24 12:26:00 -0600

    functionality works

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ let computerGameScore = 0;
 document.getElementById("rock").addEventListener("click", function(event) {
   console.log("clicked rock");
   generateComputerChoice();
-  comparison(generateComputerChoice(), this.id);
+  comparison(computerChoice2, this.id);
 });
 
 document.getElementById("paper").addEventListener("click", function(event) {
   console.log("clicked paper");
   generateComputerChoice();
-  comparison(generateComputerChoice(), this.id);
+  comparison(computerChoice2, this.id);
 });
 
 document.getElementById("scissors").addEventListener("click", function(event) {
   console.log("clicked scissors");
   generateComputerChoice();
-  comparison(generateComputerChoice(), this.id);
+  comparison(computerChoice2, this.id);
 });
 
 function generateComputerChoice() {
@@ -32,37 +32,18 @@ var computerChoice2 = generateComputerChoice()
 console.log(computerChoice2)
 
 function comparison(computerChoice2, playerChoice) {
-    if (computerChoice2 == "rock" && playerChoice == "paper") {
-      playerGameScore++
-      document.getElementById("playerGameScore").innerHTML = playerGameScore;
-      document.getElementById("result").innerHTML = "You won! Your ${playerChoice} beat the computer's ${computerChoice2}.";
-    }
-    else if (computerChoice2 == "rock" && playerChoice == "scissors") {
-      computerGameScore++
-      document.getElementById("computerGameScore").innerHTML = computerGameScore;      
-      document.getElementById("result").innerHTML = "You lost! The computer's ${computerChoice2} beat your ${playerChoice}";
-    }
-    else if (computerChoice2 == "paper" && playerChoice == "rock") {
-      computerGameScore++
-      document.getElementById("computerGameScore").innerHTML = computerGameScore;
-      document.getElementById("result").innerHTML = "You lost! The computer's ${computerChoice2} beat your ${playerChoice}";
-    }
-    else if (computerChoice2 == "paper" && playerChoice == "scissors") {
-      playerGameScore++
-      document.getElementById("playerGameScore").innerHTML = computerGameScore;
-      document.getElementById("result").innerHTML = "You won! Your ${playerChoice} beat the computer's ${computerChoice2}.";
-    }
-    else if (computerChoice2 == "scissors" && playerChoice == "rock") {
-      playerGameScore++
-      document.getElementById("playerGameScore").innerHTML = playerGameScore;
-      document.getElementById("result").innerHTML = "You won! Your ${playerChoice} beat the computer's ${computerChoice2}.";
-    }
-    else if (computerChoice2 == "scissors" && playerChoice == "paper") {
-      computerGameScore++
-      document.getElementById("computerGameScore").innerHTML = playerGameScore;
-      document.getElementById("result").innerHTML = "You lost! The computer's ${computerChoice2} beat your ${playerChoice}";
-    }
-    else if (computerChoice2 === playerChoice) {
-      document.getElementById("result").innerHTML = "It's a tie!";  
-    }   
+  if ((computerChoice2 == "rock" && playerChoice == "paper") || (computerChoice2 == "paper" && playerChoice == "scissors") || (computerChoice2 == "scissors" && playerChoice == "rock")) {
+    playerGameScore++
+    document.getElementById("playerGameScore").innerHTML = playerGameScore;
+    document.getElementById("result").innerHTML = `You won! Your ${playerChoice} beat the computers ${computerChoice2}.`;
+  }
+  else if ((computerChoice2 == "rock" && playerChoice == "scissors") || (computerChoice2 == "paper" && playerChoice == "rock") || (computerChoice2 == "scissors" && playerChoice == "paper")) {
+    computerGameScore++
+    document.getElementById("computerGameScore").innerHTML = computerGameScore;      
+    document.getElementById("result").innerHTML = `You lost! The computers ${computerChoice2} beat your ${playerChoice}`;
+  }
+  else if (computerChoice2 === playerChoice) {
+    document.getElementById("result").innerHTML = `Its a tie! You both chose ${playerChoice}!`;
+  }   
 } 
+
