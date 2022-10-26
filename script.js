2b1f2fb530025b80933ce5014972e3b2a61c4050
MasonFraley <masonfraley@hotmail.com>
2022-10-25 20:49:51 -0600

    final

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-let playerMatchScore = 0;
-let computerMatchScore = 0;
 let playerGameScore = 0;
 let computerGameScore = 0;
 
 document.getElementById("rock").addEventListener("click", function(event) {
-  comparison(computerChoice2, this.id);
+  generateComputerChoice();
+  comparison(this.id);
 });
 
 document.getElementById("paper").addEventListener("click", function(event) {
-  comparison(computerChoice2, this.id);
+  generateComputerChoice();
+  comparison(this.id);
 });
 
 document.getElementById("scissors").addEventListener("click", function(event) {
-  comparison(computerChoice2, this.id);
+  generateComputerChoice();
+  comparison(this.id);
 });
 
 function generateComputerChoice() {
@@ -22,22 +23,20 @@ function generateComputerChoice() {
   if (computerChoice === 2) return "scissors";
 }
 
-var computerChoice2 = generateComputerChoice()
-console.log(computerChoice2)
-
-function comparison(computerChoice2, playerChoice) {
+function comparison(playerChoice) {
+  var computerChoice2 = generateComputerChoice()
   if ((computerChoice2 == "rock" && playerChoice == "paper") || (computerChoice2 == "paper" && playerChoice == "scissors") || (computerChoice2 == "scissors" && playerChoice == "rock")) {
     playerGameScore++
     document.getElementById("playerGameScore").innerHTML = playerGameScore;
-    document.getElementById("result").innerHTML = `You won! Your ${playerChoice} beat the computers ${computerChoice2}.`;
+    document.getElementById("result").innerHTML = `You won! Your ${playerChoice} beat the computer\'s ${computerChoice2}.`;
   }
   else if ((computerChoice2 == "rock" && playerChoice == "scissors") || (computerChoice2 == "paper" && playerChoice == "rock") || (computerChoice2 == "scissors" && playerChoice == "paper")) {
     computerGameScore++
     document.getElementById("computerGameScore").innerHTML = computerGameScore;      
-    document.getElementById("result").innerHTML = `You lost! The computers ${computerChoice2} beat your ${playerChoice}`;
+    document.getElementById("result").innerHTML = `You lost! The computer\'s ${computerChoice2} beat your ${playerChoice}`;
   }
   else if (computerChoice2 === playerChoice) {
-    document.getElementById("result").innerHTML = `Its a tie! You both chose ${playerChoice}!`;
+    document.getElementById("result").innerHTML = `It\'s a tie! You both chose ${playerChoice}!`;
   }   
 } 
 
